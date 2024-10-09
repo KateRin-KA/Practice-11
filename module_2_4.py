389a326d65238bf78d65998c50ac1cfe1ab711ad
Klyueva Ekaterina <Kuska2006@list.ru>
2024-10-09 23:46:04 +0300

    Цикл for

---
+++ module_2_4.py
@@ -0,0 +1,15 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+for n in numbers:
+    if n > 1:
+        is_prime = True
+for i in range(2, n):
+    if n % 1 == 0:
+        is_prime = False
+        break
+    primes.append(n)
+else:
+    not_primes.append(n)
+print('Primes: ', primes)
+print('Not Primes: ', not_primes)
